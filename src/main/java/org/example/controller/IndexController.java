ba132edc5e94c0cabff5c4f61598f7137429bc99
ZhonhJingHui <2608729973@qq.com>
2024-07-21 20:41:32 +0800

    adding index and bootstrap

--- src/main/java/org/example/controller/IndexController.java
+++ src/main/java/org/example/controller/IndexController.java
@@ -3,14 +3,12 @@ package org.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller   // 允许该类接收前端请求
-public class Hello {
-    @GetMapping("/hello")
-    public String hello(@RequestParam("name")String name , Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
